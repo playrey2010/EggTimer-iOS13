de7c6b0d30521c6f1a8e1f13d2dca2d0f65768e7
Rey <playrey2010@gmail.com>
2023-02-01 17:11:38 -0800

    Done with timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,23 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
+
+// done
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer!
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    @IBOutlet weak var timerProgress: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    let eggTimes = ["Soft": 3, "Medium": 420, "Hard":  720]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
         let hardness = sender.currentTitle!
-        if hardness == "Soft" {
-            print(softTime)
-        } else if hardness == "Medium" {
-            print(mediumTime)
-        } else {
-            print(hardTime)
+        timerProgress.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        if let time = eggTimes[hardness] {
+            totalTime = time
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateCounter() {
+        if secondsPassed < totalTime {
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            secondsPassed += 1
+            print(Float(percentageProgress))
+            timerProgress.progress = Float(percentageProgress)
+            
+            
+            
+        } else if secondsPassed == totalTime {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+            timerProgress.progress = 1.0
+            playSound()
+            
         }
     }
     
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 
 }
